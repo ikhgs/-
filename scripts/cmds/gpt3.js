81a8bd2270ee7ced7c92bd65ed39e32719b73b44
ikhgs <mathematiques45@gmail.com>
2024-07-06 12:18:22 +0300

    Update gpt3.js

--- scripts/cmds/gpt3.js
+++ scripts/cmds/gpt3.js
@@ -44,7 +44,7 @@ module.exports = {
 								}, event.messageID);
 						});
 
-						const response = await axios.get(`https://api.kenliejugarap.com/freegpt4o128k/?question=${encodeURIComponent(user)}`);
+						const response = await axios.get(`https://api.kenliejugarap.com/freegpt4o8k/?question=${encodeURIComponent(user)}`);
 
 						const responseData = response.data.response;
 						const cleanResponseData = responseData.replace(/\n\nIs this answer helpful to you\? Kindly click the link below\nhttps:\/\/click2donate.kenliejugarap.com\n\(Clicking the link and clicking any ads or button and wait for 30 seconds \(3 times\) everyday is a big donation and help to us to maintain the servers, last longer, and upgrade servers in the future\)/, '');
